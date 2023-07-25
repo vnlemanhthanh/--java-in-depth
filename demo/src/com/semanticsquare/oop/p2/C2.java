8409500fcff200a57aa270c3242bfe1721a9c9be
Thanh LE <vnlemanhthanh@gmail.com>
2023-07-26 03:13:14 +0700

    access modifier

---
+++ demo/src/com/semanticsquare/oop/p2/C2.java
@@ -0,0 +1,12 @@
+package com.semanticsquare.oop.p2;
+
+import com.semanticsquare.oop.p1.A1;
+
+public class C2 extends A1 {
+	public static void main(String[] args) {
+		//System.out.println("privateMember: " + privateMember);
+		//System.out.println("defaultMember: " + defaultMember);
+		System.out.println("protectedMember: " + protectedMember);
+		System.out.println("publicMember: " + publicMember);
+	}
+}
