916318cd49c77836307faed4349f15e09e8b0ed1
Thanh LE <vnlemanhthanh@gmail.com>
2023-07-26 22:12:33 +0700

    boolean datatype

--- javaindepth/src/com/semanticsquare/basics/BasicsDemo.java
+++ javaindepth/src/com/semanticsquare/basics/BasicsDemo.java
@@ -1,30 +1,32 @@
-class BasicsDemo {
-  int id1 = 0;
-  int id = 0;
-  int Id = 0;
-
-  void foo() {;};
-
-
-  static void print() {
-    System.out.println("\nInside print() ...");
-
-  }
-
-  // client test
-  public static void main(String[] args) {
-    print();
-
-    int i =6;
-    double j =2.5;
-    System.out.println(i + j);
-    System.out.println(i - j);
-    System.out.println(i * j);
-    System.out.println(i / j);
-    System.out.println(i % j);
-    // System.out.println(i ** j);
-
-
-  }
-
+  class BasicsDemo {
+      // Adapted from http://www.ntu.edu.sg/home/ehchua/programming/java/J1a_Introduction.html
+      static void print() {
+          System.out.println("\n\nInside print ...");
+          System.out.println("Hello, world!!"); // Advance cursor to beginning of next line
+          System.out.println();                 // Print empty line
+          System.out.print("Hello, world!!");   // Cursor stayed after the printed string
+          System.out.println("Hello,");
+          System.out.print(" ");                // Print a space
+          System.out.print("world!!");
+      }
+	  
+	  static void primitives() {
+	      System.out.println("\n\nInside primitives ...");
+		  int intHex = 0x0041; // 16 power 0 * 1 + 16 power 1 * 4
+		  System.out.println("intHex: " + intHex);
+		  
+		  // Java 7
+		  int intBinary = 0b0100_0001;
+			int dfd = 0b0110101_1111;
+		  System.out.println("intBinary: " + dfd);
+		  
+		  int intOctal = 0101;
+		  System.out.println("intOctal: " + intOctal);
+	  }
+	  
+	  public static void main(String[] args) {	
+	   	  // Language Basics 1
+		  //print();	  
+		  primitives();
+      }       
 }
--- javaindepth/src/com/semanticsquare/basics/Student.java
+++ javaindepth/src/com/semanticsquare/basics/Student.java
@@ -1,15 +1,37 @@
-class Student {
-  int id = 1000;;
 
-  void compute() {
-  int nextId = id + 100;
-    System.out.println(id + " - id");
-    System.out.println("nextId: " + nextId);
-  }
-
-  public static void main(String[] agrs) {
-    Student s = new Student();
-    s.compute();
-  }
-
+public class Student {
+	int id = 1000;
+	
+	byte age = 18;
+	long phone = 223_456_7890L;
+	
+	double gpa = 3.8; 
+	
+	char degree = 'B';
+	
+	boolean international = true;
+	double tuitionFees = 12000.0;
+	double internationalFees = 5000.0;
+			
+	void compute() {
+		int nextId = id+1;
+		
+		if (international) {
+			tuitionFees = tuitionFees + internationalFees;
+		}
+		
+		System.out.println("id: " + id);
+		System.out.println("nextId: " + nextId);		
+		System.out.println("age: " + age);
+		System.out.println("phone: " + phone);		
+		System.out.println("gpa: " + gpa);		
+		System.out.println("degree: " + degree);
+		
+		System.out.println("tuitionFees: " + tuitionFees);
+	}
+	
+	public static void main(String[] args) {
+		Student s = new Student();
+		s.compute();
+	}
 }
