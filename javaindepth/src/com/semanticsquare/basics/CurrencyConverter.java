584cab49fd831b193d3511f7b34c8a90accf9c33
Thanh LE <vnlemanhthanh@gmail.com>
2023-07-27 20:44:18 +0700

    assignment 2

--- javaindepth/src/com/semanticsquare/basics/CurrencyConverter.java
+++ javaindepth/src/com/semanticsquare/basics/CurrencyConverter.java
@@ -1,25 +1,28 @@
 class CurrencyConverter {
-  int rupee = 63;
-  int dirham = 3; // UAE
-  int real = 3;  // brazilian     
-  int chilean_peso = 595;
-  int mexican_peso = 18;       
-  int _yen = 107;
-  int $australian = 2;  // australian dollar
-  int dollar;
-  int Rupee = 63;
+
+  double[] exchangeRates = new double[7];
+  double rupee = exchangeRates[0] = 63.0;
+  double dirham = exchangeRates[1] = 3.0; // UAE
+  double real = exchangeRates[2] = 3.0;  // brazilian     
+  double chilean_peso = exchangeRates[3] = 595.0;
+  double mexican_peso = exchangeRates[4] = 18.0;       
+  double _yen = exchangeRates[5] = 107.0;
+  double $australian = exchangeRates[6] = 2.0;  // australian dollar
+  // int dollar;
+  // int Rupee = 63;
 
   void printCurrencies() {
 
-   System.out.println("rupee: " + rupee);   
-   System.out.println("dirham: " + dirham);
-   System.out.println("real: " + real);
-   System.out.println("chilean_peso: " + chilean_peso);
-   System.out.println("mexican_peso: " + mexican_peso);
-   System.out.println("_yen: " + _yen);
-   System.out.println("$australian: " + $australian);
-   System.out.println("dollar: " + dollar);
-   System.out.println("Rupee: " + Rupee);
+   System.out.println("rupee: " + exchangeRates[0]);   
+   System.out.println("dirham: " + exchangeRates[1]);
+   System.out.println("real: " + exchangeRates[2]);
+   System.out.println("chilean_peso: " + exchangeRates[3]);
+   System.out.println("mexican_peso: " + exchangeRates[4]);
+   System.out.println("_yen: " + exchangeRates[5]);
+   System.out.println("$australian: " + exchangeRates[exchangeRates.length - 1]);
+   System.out.println("# exchangeRates length: " + exchangeRates.length);
+  //  System.out.println("dollar: " + dollar);
+  //  System.out.println("Rupee: " + Rupee);
   }
 
   public static void main(String[] ars) {
