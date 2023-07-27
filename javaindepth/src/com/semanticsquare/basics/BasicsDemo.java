4dae346783b1f334d139c12721ebec4f93774f59
Thanh LE <vnlemanhthanh@gmail.com>
2023-07-28 05:59:12 +0700

    test

--- javaindepth/src/com/semanticsquare/basics/BasicsDemo.java
+++ javaindepth/src/com/semanticsquare/basics/BasicsDemo.java
@@ -68,41 +68,51 @@
 	  
 	  
       static void arrays() {
-	      System.out.println("\nInside arrays ...");
-		  int[] scores = new int[4];	
+	   System.out.println("\nInside arrays ...");
+	   int[] myArray = new int[]{9, 11, 2, 5, 4, 4, 6};
+       System.out.println("myArray.length: " + myArray.length);
+	   System.out.println("myArray[1]: " + myArray[1]);
+	   System.out.println("myArray[7]: " + myArray[7]);	   
+	}
 
-		  scores[0] = 90;
-		  scores[1] = 70;
-		  scores[2] = 80;
-		  scores[3] = 100;
-		  
-		  /*int[] scores = new int[] {90, 70, 80, 100};*/
-		  
-		  //int[] scores = {90, 70, 80, 100};
-		  
-		  System.out.println("Mid-Term 1: " + scores[0]);
-		  System.out.println("Mid-Term 2: " + scores[1]);
-		  System.out.println("Final: " + scores[2]);
-		  System.out.println("Project: " + scores[3]);
-		  System.out.println("# exams: " + scores.length);
-		  
-		  Student[] students = new Student[3];//{new Student(), new Student(), new Student()};
-		  students[0] = new Student();
-		  students[1] = new Student();
-		 // students[2] = new Student();
-		  /*students[0].name = "John";
-		  students[1].name = "Raj";
-		  students[2].name = "Anita";*/
-		  System.out.println("Student 1: " + students[0]);
-		  System.out.println("Student 2: " + students[1]);
-		  System.out.println("Student 3: " + students[2]);
-	  }
-	  
-	  public static void main(String[] args) {	
-	   	  // Language Basics 1
-		  //print();	  
-		  //primitives();
-		  //typeCasting();	
-          arrays();		  
-	  }       
-}
+    static void threeDimensionalArrays() {
+	   System.out.println("\nInside threeDimensionalArrays ...");
+	   int[][][] unitsSold = new int[][][]{ 
+										   { // New York
+											 {0,0,0,0}, // Jan
+											 {0,0,0,0}, // Feb
+											 {0,0,0,0}, // Mar
+											 {0,850,0,0}// Apr	
+										   },
+										   { // San Francisco
+											 {0,0,0,0}, // Jan
+											 {0,0,0,0}, // Feb
+											 {0,0,0,0}, // Mar
+											 {0,0,0,0}  // Apr
+										   },
+										   { 
+											 {0,0,0,0},
+											 {0,0,0,0},
+											 {0,0,0,0},
+											 {0,0,0,0}
+										   },
+										   {
+											 {0,0,0,0},
+											 {0,0,0,0},
+											 {0,0,0,0},
+											 {0,0,0,0}
+										   }
+										 };
+		                    	 
+		System.out.println("unitsSold[0][3][1]: " + unitsSold[0][3][1]);
+	}	
+  
+    public static void main(String[] args) {	
+	  // Language Basics 1
+	  //print();				
+	  //primitives();
+	  //typeCasting();
+	  //arrays();	
+      threeDimensionalArrays();	  
+    }       
+  }
