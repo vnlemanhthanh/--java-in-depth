86e620cbecad1f31112ebd40bd3f5dbd4367e207
Thanh LE <vnlemanhthanh@gmail.com>
2023-08-01 00:04:03 +0700

    string pool & interning

--- javaindepth/src/com/semanticsquare/basics/BasicsDemo.java
+++ javaindepth/src/com/semanticsquare/basics/BasicsDemo.java
@@ -376,6 +376,23 @@
 		System.out.println("\nString.valueOf(1.3): " + String.valueOf(1.3));
 	}
 		
+	static void stringPool() {
+	  System.out.println("\nInside stringPool ...");
+	  String s1 = "hello!";
+	  String s2 = "hello!";
+	  String s3 = "hello!".intern();
+	  String s4 = new String("hello!");	 
+	  final String s5 = "lo!";
+
+      System.out.println("s1 == s2: " + (s1 == s2));
+      System.out.println("s1 == s3: " + (s1 == s3));
+      System.out.println("s1 == s4: " + (s1 == s4));
+      System.out.println("s1 == s4.intern(): " + (s1 == s4.intern()));
+      System.out.println("s1 == \"hel\" + \"lo!\": " + (s1 == "hel" + "lo!"));
+      System.out.println("s1 == \"hel\" + s5: " + (s1 == "hel" + s5));
+  }
+
+
 	public static void main(String[] args) {	
 	  // Language Basics 1
 	  //print();				
@@ -393,6 +410,7 @@
 	  //labeledContinue();
       // import demo
       // package creation demo
-      stringExamples();	  
+      //stringExamples();	  
+			stringPool();
     }       
 }
