dc32576a6648c6efc991e0d51f3d2803f8f5c500
Thanh LE <vnlemanhthanh@gmail.com>
2023-08-05 22:09:21 +0700

    debug

---
+++ javaindepth/src/com/semanticsquare/basics/debug/Department.java
@@ -0,0 +1,18 @@
+package com.semanticsquare.basics.debug;
+
+public class Department {
+    private String name;
+
+	public Department(String departmentName) {
+		this.name = departmentName;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
+
