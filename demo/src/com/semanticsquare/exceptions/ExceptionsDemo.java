659d4a8a9fd75a9fd431fb65d8b52a2d2569429f
Thanh LE <vnlemanhthanh@gmail.com>
2023-08-07 20:40:33 +0700

    Exceptions

---
+++ demo/src/com/semanticsquare/exceptions/ExceptionsDemo.java
@@ -0,0 +1,37 @@
+package com.semanticsquare.exceptions;
+
+import java.io.FileNotFoundException;
+
+public class ExceptionsDemo {
+
+    public static void main(String[] args) throws FileNotFoundException {
+	System.out.println("\nInside main ...");
+
+	share();
+//	try {
+//	    share();
+//	} catch (FileNotFoundException e) {
+//	    // TODO Auto-generated catch block
+//	    e.printStackTrace();
+//	}
+
+	System.out.println("\nEnd of main ...");
+    }
+
+    private static void share() throws FileNotFoundException {
+	System.out.println("\nInside share ...");
+	
+	// HttpConnect.send(0, "hello", "http://www.goodsnips.com");
+	
+	try {
+	    HttpConnect.send(0, "hello", "http://www.goodsnips.com");
+
+	} catch (FileNotFoundException e) {
+	    // TODO Auto-generated catch block
+	    throw e;
+	}
+
+	System.out.println("\nEnd of share ...");
+    }
+
+}
---
+++ demo/src/com/semanticsquare/exceptions/HttpConnect.java
@@ -0,0 +1,17 @@
+package com.semanticsquare.exceptions;
+
+import java.io.FileNotFoundException;
+
+public class HttpConnect  {
+
+    public static void send(int destination, String data, String partner) 
+	    throws FileNotFoundException {
+	System.out.println("\nInside send ...");
+	
+	if (destination == 0) {
+	    throw new FileNotFoundException(/* "No file" */);
+	}
+
+	System.out.println("\nEnd of send ...");
+    }
+}
