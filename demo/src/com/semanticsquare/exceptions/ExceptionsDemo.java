56a2921a120bec107282c2cbbeb46dd7e65f1174
Thanh LE <vnlemanhthanh@gmail.com>
2023-08-07 22:39:02 +0700

    finally { }

--- demo/src/com/semanticsquare/exceptions/ExceptionsDemo.java
+++ demo/src/com/semanticsquare/exceptions/ExceptionsDemo.java
@@ -1,34 +1,40 @@
 package com.semanticsquare.exceptions;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class ExceptionsDemo {
 
     public static void main(String[] args) throws FileNotFoundException {
 	System.out.println("\nInside main ...");
-
-	share();
-//	try {
-//	    share();
-//	} catch (FileNotFoundException e) {
-//	    // TODO Auto-generated catch block
-//	    e.printStackTrace();
-//	}
-
+	try {
+	    share();
+	    System.out.println("After invoking share ...");
+	} catch (FileNotFoundException e) {
+	    // TODO Auto-generated catch block
+	    // e.printStackTrace();
+	    System.out.println("main: filenotfoundexception ...");
+	} finally {
+	    System.out.println("Inside main's finally ...");
+	}
 	System.out.println("\nEnd of main ...");
     }
 
     private static void share() throws FileNotFoundException {
 	System.out.println("\nInside share ...");
-	
-	// HttpConnect.send(0, "hello", "http://www.goodsnips.com");
-	
+
 	try {
 	    HttpConnect.send(0, "hello", "http://www.goodsnips.com");
-
+	    System.out.println("After invoking send ...");
 	} catch (FileNotFoundException e) {
 	    // TODO Auto-generated catch block
+	    System.out.println("Share: filenotfoundexception ...");
 	    throw e;
+	} catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    System.out.println("Connecting to a different server ...");
+	} finally {
+	    System.out.println("Inside share's finally ...");
 	}
 
 	System.out.println("\nEnd of share ...");
--- demo/src/com/semanticsquare/exceptions/HttpConnect.java
+++ demo/src/com/semanticsquare/exceptions/HttpConnect.java
@@ -1,15 +1,30 @@
 package com.semanticsquare.exceptions;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
-public class HttpConnect  {
+public class HttpConnect {
 
-    public static void send(int destination, String data, String partner) 
-	    throws FileNotFoundException {
+    /**
+     * 
+     * @param destination
+     * @param data
+     * @param partner
+     * @throws IOException
+     * @throws IllegalArgumentException generated if destination < 0 ||
+     *                                  destination > 1
+     */
+    public static void send(int destination, String data, String partner)
+	    throws IOException {
 	System.out.println("\nInside send ...");
-	
+	if (destination < 0 || destination > 1) {
+	    throw new IllegalArgumentException();
+	}
+
 	if (destination == 0) {
-	    throw new FileNotFoundException(/* "No file" */);
+	    throw new FileNotFoundException();
+	} else if (destination == 1) {
+	    throw new IOException();
 	}
 
 	System.out.println("\nEnd of send ...");
