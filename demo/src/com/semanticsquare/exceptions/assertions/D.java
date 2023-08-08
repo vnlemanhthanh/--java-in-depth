84dd1e7cc97d38d0006b8b7b6297a8afa66e201c
Thanh LE <vnlemanhthanh@gmail.com>
2023-08-08 22:26:07 +0700

    assertions

---
+++ demo/src/com/semanticsquare/exceptions/assertions/D.java
@@ -0,0 +1,11 @@
+package com.semanticsquare.exceptions.assertions;
+
+public class D {
+	
+	void test(int i) {
+		assert i > 0 : "invalid i in D.test";
+		
+		B b = new B();
+		b.test(-1);
+	}
+}
---
+++ demo/src/com/semanticsquare/exceptions/assertions/p1/C.java
@@ -0,0 +1,8 @@
+package com.semanticsquare.exceptions.assertions.p1;
+
+public class C {
+	
+	public void test(int i) {
+		assert i > 0 : "invalid i in C.test";
+	}
+}
