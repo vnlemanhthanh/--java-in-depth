159e3fc83980b9efc757f7583706a83d885a5a1e
Thanh LE <vnlemanhthanh@gmail.com>
2023-08-16 22:25:17 +0700

    test

--- demo/src/com/semanticsquare/collections/ListDemo.java
+++ demo/src/com/semanticsquare/collections/ListDemo.java
@@ -9,7 +9,7 @@ public class ListDemo {
 	List<Integer> list1 = new ArrayList<>();
 	list1.add(1);
 	list1.add(2);
-	list1.add(3);
+	list1.add(3); 
 	list1.add(3);
 	list1.add(null);
 	System.out.println("list1: " + list1);
